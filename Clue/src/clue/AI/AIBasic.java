911af13ff61bb285e2d8802954a683893e9d01d4
Zemiguel99 <48128161+Zemiguel99@users.noreply.github.com>
2019-04-05 14:38:53 +0100

    Added comments.
    Added comments to AIBasic.java

--- Clue/src/clue/AI/AIBasic.java
+++ Clue/src/clue/AI/AIBasic.java
@@ -58,20 +58,20 @@ public class AIBasic extends AIPlayer{
         WeaponCard randWeaponCard = new WeaponCard(rand.nextInt(9)+ 1);
         
         
-        if(gameController.getPlayer().getId() == getId() && gameController.getLastAction() instanceof EndTurnAction){
+        if(gameController.getPlayer().getId() == getId()){//If I need to respond.
+            if(gameController.getLastAction() instanceof EndTurnAction){//If my turn, last action was end turn.
+                if(this.getPosition().isRoom()){//If I'm in a room 
+                    sendAction(new AccuseAction(this, randPersonCard, randRoomCard, randWeaponCard));
+                }
+                
+            } else if (gameController.getLastAction() instanceof ShowCardsAction){//If I need to show a card 
 
-            if(this.getPosition().isRoom()){
-                sendAction(new AccuseAction(this, randPersonCard, randRoomCard, randWeaponCard));
+                ShowCardsAction action = (ShowCardsAction) gameController.getLastAction();
+                Card card = action.getCardList().get(0);
+                ShowCardAction newAction = new ShowCardAction(action.getSuggester(), card);
+
+                sendAction(newAction);//Show Card.
             }
-        } else if (gameController.getPlayer().getId() == getId() && gameController.getLastAction() instanceof ShowCardsAction){
-        
-            ShowCardsAction action = (ShowCardsAction) gameController.getLastAction();
-            
-            Card card = action.getCardList().get(0);
-            
-            ShowCardAction newAction = new ShowCardAction(action.getSuggester(), card);
-            
-            sendAction(newAction);
         }
     }
     
