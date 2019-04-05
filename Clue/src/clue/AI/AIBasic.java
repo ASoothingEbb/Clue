26dc3ca4a6ec314d79d4a448b1b073a02fc6583a
Zemiguel99 <48128161+Zemiguel99@users.noreply.github.com>
2019-04-05 14:26:51 +0100

    Basic AI

--- Clue/src/clue/AI/AIBasic.java
+++ Clue/src/clue/AI/AIBasic.java
@@ -5,10 +5,74 @@
  */
 package clue.AI;
 
+import clue.GameController;
+import clue.action.AccuseAction;
+import clue.action.Action;
+import clue.action.EndTurnAction;
+import clue.action.ShowCardAction;
+import clue.action.ShowCardsAction;
+import clue.card.Card;
+import clue.card.PersonCard;
+import clue.card.RoomCard;
+import clue.card.WeaponCard;
+import clue.player.AIPlayer;
+import clue.player.Player;
+import clue.tile.Tile;
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author zemig
  */
-public class AIBasic {
+public class AIBasic extends AIPlayer{
+    
+//    private boolean active;
+//    private List<Card> cards;
+//    private Tile position;
+//    private int movements;
+//    private int id;
+//    private GameController game;
+    
+    private GameController gameController;
+    Random rand;
+    
+    
+    
+    
+    public AIBasic(int id) {
+        super(id);
+        
+        gameController = getGameController();
+        rand = new Random();
+    }
+    
+    
+    
+    
+    public void onUpdate() {
+       
+        
+        PersonCard randPersonCard = new PersonCard(rand.nextInt(6) + 1);
+        RoomCard randRoomCard = new RoomCard(rand.nextInt(6) + 1);
+        WeaponCard randWeaponCard = new WeaponCard(rand.nextInt(9)+ 1);
+        
+        
+        if(gameController.getPlayer().getId() == getId() && gameController.getLastAction() instanceof EndTurnAction){
+
+            if(this.getPosition().isRoom()){
+                sendAction(new AccuseAction(this, randPersonCard, randRoomCard, randWeaponCard));
+            }
+        } else if (gameController.getPlayer().getId() == getId() && gameController.getLastAction() instanceof ShowCardsAction){
+        
+            ShowCardsAction action = (ShowCardsAction) gameController.getLastAction();
+            
+            Card card = action.getCardList().get(0);
+            
+            ShowCardAction newAction = new ShowCardAction(action.getSuggester(), card);
+            
+            sendAction(newAction);
+        }
+    }
     
 }
