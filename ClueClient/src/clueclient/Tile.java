3261927e20a5a964d698e5702b6790ba292bfcbc
Marumaroo <hungbuiquang2@gmail.com>
2019-04-06 16:46:01 +0100

    created tile class for board

---
+++ ClueClient/src/clueclient/Tile.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clueclient;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+
+/**
+ *
+ * @author hungb
+ */
+public class Tile extends Rectangle {
+    
+    private Player player;
+    
+    public Tile(int size) {
+        setWidth(size);
+        setHeight(size);
+        
+        setStroke(Color.BLACK);
+        setStrokeType(StrokeType.INSIDE);
+        //setFill(Color.BROWN);
+        
+        setFill(Color.rgb(200, 200, 200, 0.5));
+    }
+    
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+    
+    public void removePlayer(Player player) {
+        this.player = null;
+    }
+}
