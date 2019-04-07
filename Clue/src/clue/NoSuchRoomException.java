cf4dac243012450f17756624e17628524dd4591e
Mathew Wright <mw434@sussex.ac.uk>
2019-04-07 14:16:43 +0100

    +added NoSuchRoomException class

---
+++ Clue/src/clue/NoSuchRoomException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clue;
+
+/**
+ *
+ * @author Malter
+ */
+public class NoSuchRoomException extends Exception {
+
+    public NoSuchRoomException(String message) {
+        super(message);
+    }
+    
+    public NoSuchRoomException() {
+    }
+    
+}
