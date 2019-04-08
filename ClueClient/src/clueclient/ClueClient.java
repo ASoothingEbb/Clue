325464be1eb07ad7839971c20e22f683f50e5164
Marumaroo <hungbuiquang2@gmail.com>
2019-04-08 18:42:15 +0100

    Added master Volume Slider, Texture Settings, Credits, Styled How To Play Scene

--- ClueClient/src/clueclient/ClueClient.java
+++ ClueClient/src/clueclient/ClueClient.java
@@ -6,9 +6,16 @@
 package clueclient;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Properties;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -55,6 +62,8 @@ public class ClueClient extends Application {
     private int width;
     private int height;
     private String currentWindowMode;
+    
+    private HashMap<String, String> textureMap;
 
     // Fonts
     private final Font avenirLarge = Font.loadFont(getClass().getResourceAsStream("assets/fonts/Avenir-Book.ttf"), 30);
@@ -122,16 +131,12 @@ public class ClueClient extends Application {
         alignmentPane.setCenter(startGameOptions);
         
         // Scene Title
-        Label startGameTitle = new Label("Create Game");
-        startGameTitle.setFont(avenirTitle);
-        startGameTitle.setTextFill(Color.WHITE);
-
+        Label startGameTitle = getLabel("Create Game", avenirTitle);
+        
         // Number of Players
         final int numberOfPlayers = 6;
         
-        Label numberOfPlayersLabel = new Label("Number of Players");
-        numberOfPlayersLabel.setFont(avenirNormal);
-        numberOfPlayersLabel.setTextFill(Color.WHITE);
+        Label numberOfPlayersLabel = getLabel("Number of Players", avenirNormal);
         
         Slider playersNum = new Slider(1,numberOfPlayers,6);
         
@@ -142,10 +147,8 @@ public class ClueClient extends Application {
         playersNum.setSnapToTicks(true);
 
         // Number of AI Players
-        Label aiPlayersLabel = new Label("AI Players");
-        aiPlayersLabel.setFont(avenirNormal);
-        aiPlayersLabel.setTextFill(Color.WHITE);
-        
+        Label aiPlayersLabel = getLabel("AI Players", avenirNormal);
+
         CheckBox aiPlayers = new CheckBox();
         
         // Create Game Instance
@@ -187,15 +190,19 @@ public class ClueClient extends Application {
     }
     
     private void howToPlayScene(Stage stage) {
-        GridPane joinGameOptions = new GridPane();
-        joinGameOptions.setAlignment(Pos.CENTER);
+        GridPane howToPlayLayout = new GridPane();
+        howToPlayLayout.setAlignment(Pos.CENTER);
+        howToPlayLayout.setBackground(blackFill);
+        
+        Label howToPlayTitle = getLabel("How To Play", avenirTitle);
+        
+        MenuItem returnButton = new MenuItem("Back", avenirTitle);
+        returnButton.setOnMouseClicked(e -> stage.setScene(prevScene));
+        
+        howToPlayLayout.add(howToPlayTitle, 0, 0);
+        howToPlayLayout.add(returnButton, 0, 1);
 
-        // Scene Title
-        Label howToPlayTitle = new Label("How to Play");
-
-        joinGameOptions.add(howToPlayTitle, 0,0);
-
-        Scene scene = new Scene(joinGameOptions, width, height);
+        Scene scene = new Scene(howToPlayLayout, width, height);
         prevScene = stage.getScene();
         stage.setScene(scene);
     }
@@ -213,13 +220,13 @@ public class ClueClient extends Application {
         textureSettings.setPadding(new Insets(20,30,20,30));
         GridPane audioSettings = new GridPane();
         audioSettings.setBackground(new Background(new BackgroundFill(Color.rgb(50, 50, 50), CornerRadii.EMPTY, Insets.EMPTY)));
+        audioSettings.setPadding(new Insets(20,30,20,30));
         GridPane creditsPane = new GridPane();
         creditsPane.setBackground(new Background(new BackgroundFill(Color.rgb(50, 50, 50), CornerRadii.EMPTY, Insets.EMPTY)));
+        creditsPane.setPadding(new Insets(20,30,20,30));
         
         
-        Label settingsTitle = new Label("Settings");
-        settingsTitle.setTextFill(Color.WHITE);
-        settingsTitle.setFont(avenirLarge);
+        Label settingsTitle = getLabel("Settings", avenirLarge);
         
         MenuItem texturesButton = new MenuItem("Textures", avenirTitle);
         texturesButton.setOnMouseClicked(e -> {
@@ -243,36 +250,21 @@ public class ClueClient extends Application {
         returnButton.setOnMouseClicked(e -> stage.setScene(prevScene));
         
         // Textures
-        Label boardTextureLabel = new Label("Board");
-        boardTextureLabel.setTextFill(Color.WHITE);
-        boardTextureLabel.setFont(avenirTitle);
         
-        TextField filePath = new TextField();
-        filePath.setEditable(false);        
-        filePath.setPrefColumnCount(40);
+        textureSettingsScene(textureSettings);
         
-        ExtensionFilter imageFormats = new ExtensionFilter("*.jpg", "*.png", "*.jpeg");
+        // Audio
         
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setSelectedExtensionFilter(imageFormats);
-        fileChooser.setTitle("Select");
-
-        MenuItem fileSelector = new MenuItem("Choose", avenirTitle);
-        fileSelector.setOnMouseClicked(e -> {
-            File boardTexture = fileChooser.showOpenDialog(stage);
-            filePath.setText(boardTexture.getAbsolutePath());
-        });
+        audioSettingsScene(audioSettings);
+        
+        // Credits
+        
+        creditsScene(creditsPane);
         
         // adding nodes to panes
         
         settingsOptions.getChildren().addAll(texturesButton, audioButton, creditsButton);
         settingsOptions.setPadding(new Insets(0,10,0,20));
-        
-        textureSettings.add(boardTextureLabel, 0, 0);
-        GridPane.setMargin(boardTextureLabel, new Insets(0,10,0,0));
-        textureSettings.add(filePath, 1, 0);
-        textureSettings.add(fileSelector, 2, 0);
-        GridPane.setMargin(fileSelector, new Insets(0,0,0,10));
         
         settingsLayout.setLeft(leftLayout);
         
@@ -287,7 +279,175 @@ public class ClueClient extends Application {
         Scene scene = new Scene(settingsLayout, width, height);
         prevScene = stage.getScene();
         stage.setScene(scene);
-   }
+    }
+    
+    private GridPane creditsScene(GridPane layout) {      
+        LinkedHashMap <String, String> credits = new LinkedHashMap<>();
+        credits.put("Produced By", "Big Sage Productions");
+        credits.put("Project Manager", "Jak and Whip");
+        credits.put("AI", "Jose");
+        credits.put("Lead Programmer", "Steve");
+        credits.put("Programmer", "Mathew");
+        credits.put(" ", "Jak");
+        credits.put("  ", "Jose");
+        credits.put("Artist", "Hung");
+        credits.put("UX Design","Hung");
+        credits.put("Composer", "What Music XD");
+        credits.put("Tester", "Your Nan");
+        credits.put("Yasuo", "DAB");
+        credits.put("Special Thanks to", "Big Sage");
+        
+        int index = 1;
+        for(Map.Entry<String, String> entry: credits.entrySet()) {
+            Label title = getLabel(entry.getKey(), avenirTitle);
+            Label credit = getLabel(entry.getValue(), avenirTitle);
+            
+            layout.add(title, 0, index);
+            layout.add(credit, 1, index);
+            GridPane.setMargin(credit, new Insets(0, 0, 0, 50));
+            index++;
+        }
+        
+        layout.setAlignment(Pos.CENTER);
+        
+        return layout;
+    }
+    
+    private GridPane audioSettingsScene(GridPane layout) {
+        Label masterLabel = getLabel("Master Volume", avenirTitle);
+        
+        Slider masterVolume = new Slider(0, 100, 100);
+        masterVolume.setMaxWidth(1000);
+        masterVolume.setBlockIncrement(10);
+        Label masterVolumeShow = getLabel("", avenirTitle);
+        masterVolumeShow.setText("100");
+                
+        masterVolume.valueProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                masterVolumeShow.setText(String.valueOf((int) masterVolume.getValue()));
+            }
+        });
+        
+        layout.add(masterLabel, 0, 0);
+        GridPane.setMargin(masterLabel, new Insets(0, 10, 0, 0));
+        layout.add(masterVolume, 1, 0);
+        layout.add(masterVolumeShow, 2, 0);
+        GridPane.setMargin(masterVolumeShow, new Insets(0, 0, 0, 10));
+        
+        return layout;
+    }
+    
+    private GridPane textureSettingsScene(GridPane layout) {
+        textureMap = new HashMap<>();
+        
+        ExtensionFilter imageFormats = new ExtensionFilter("Image files", "*.jpg", "*.png", "*.jpeg");
+        
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(imageFormats);
+        fileChooser.setTitle("Select");
+        
+        Label board = getLabel("Board", avenirTitle);
+        TextField boardFilePath = getTextField(40, false);
+        
+        MenuItem selectBoardFile = new MenuItem("Choose", avenirTitle);
+        selectBoardFile.setOnMouseClicked(e -> {
+            File boardTexture = fileChooser.showOpenDialog(stage);
+            boardFilePath.setText(boardTexture.getAbsolutePath());
+            textureMap.put("BoardTexture",boardTexture.getAbsolutePath());
+        });
+        
+        layout.add(board, 0, 0);
+        GridPane.setMargin(board, new Insets(0, 10, 0, 0));
+        layout.add(boardFilePath, 1, 0);
+        layout.add(selectBoardFile, 2, 0);
+        GridPane.setMargin(selectBoardFile, new Insets(0, 0, 0, 10));
+
+        for (int i=1; i < 7; i++) {
+            Label character = getLabel("Character " + i, avenirTitle);
+            TextField characterFilePath = getTextField(40, false);
+            
+            final String propertiesCharacter = "Character"+i+"Texture";
+            
+            MenuItem selectCharacterFile = new MenuItem("Choose", avenirTitle);
+            selectCharacterFile.setOnMouseClicked(e -> {
+                File characterTexture = fileChooser.showOpenDialog(stage);
+                characterFilePath.setText(characterTexture.getAbsolutePath());
+                textureMap.put(propertiesCharacter,characterTexture.getAbsolutePath());
+            });
+            
+            layout.add(character, 0, i);
+            GridPane.setMargin(character, new Insets(0, 10, 0, 0));
+            layout.add(characterFilePath, 1, i);
+            layout.add(selectCharacterFile, 2, i);
+            GridPane.setMargin(selectCharacterFile, new Insets(0, 0, 0, 10));
+        }
+        
+        for (int i=1; i < 7; i++) {
+            Label weapon = getLabel("Weapon " + i, avenirTitle);
+            TextField weaponFilePath = getTextField(40, false);
+            
+            final String propertiesWeapon = "Weapon"+i+"Texture";
+            MenuItem selectWeaponFile = new MenuItem("Choose", avenirTitle);
+            selectWeaponFile.setOnMouseClicked(e -> {
+                File weaponTexture = fileChooser.showOpenDialog(stage);
+                weaponFilePath.setText(weaponTexture.getAbsolutePath());
+                textureMap.put(propertiesWeapon,weaponTexture.getAbsolutePath());
+            });
+            
+            layout.add(weapon, 0, i + 6);
+            GridPane.setMargin(weapon, new Insets(0, 10, 0, 0));
+            layout.add(weaponFilePath, 1, i + 6);
+            layout.add(selectWeaponFile, 2, i + 6);
+            GridPane.setMargin(selectWeaponFile, new Insets(0, 0, 0, 10));
+        }
+        
+        MenuItem applyChanges = new MenuItem("Apply", avenirTitle);
+        applyChanges.setOnMouseClicked(e -> saveProperties());
+        
+        layout.add(applyChanges, 2, 14);
+        GridPane.setMargin(applyChanges, new Insets(0, 0, 0, 15));
+        
+        return layout;
+    }
+    
+    // TODO: NullPointerException - can't load properties file
+    private boolean saveProperties() {
+        try {
+            InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("assets/config.properties");
+            String path = getClass().getResource("assets/config.properties").toString();
+            File file = new File("assets/config.properties");
+            try (FileInputStream fileInput = new FileInputStream(path)) {
+                Properties properties = new Properties();
+                properties.load(stream);
+                
+                textureMap.entrySet().forEach((entry) -> {
+                    properties.put(entry.getKey(), entry.getValue());
+                });
+                
+                Prompt confirmPrompt = new Prompt("Changes Saved");
+                confirmPrompt.showAndWait();
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    private TextField getTextField(int columnCount, boolean editable) {
+        TextField filePath = new TextField();
+        filePath.setPrefColumnCount(columnCount);
+        filePath.setEditable(editable);
+        return filePath;        
+    }
+    
+    private Label getLabel(String text, Font font) {
+        Label label = new Label(text);
+        label.setTextFill(Color.WHITE);
+        label.setFont(font);
+        return label;
+    }
 
     /**
      * @param args the command line arguments
