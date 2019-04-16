03dc068ba1aa7740233fd0921d04dd4688f638b0
Marumaroo <hungbuiquang2@gmail.com>
2019-04-16 17:43:15 +0100

    separating customboardmaker

---
+++ Clue/src/clue/client/CreationException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clue.client;
+
+/**
+ *
+ * @author hungb
+ */
+public class CreationException extends Exception {
+    public String message;
+    
+    public CreationException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
