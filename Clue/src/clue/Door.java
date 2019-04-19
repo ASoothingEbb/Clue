6bd5e9d9416c77d3a1da749db49b8c4707056d95
stephen-bracken <stephen.bracken20@gmail.com>
2019-04-19 18:15:27 +0100

    moved Door to clue main package

---
+++ Clue/src/clue/Door.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clue;
+
+import clue.tile.Room;
+import clue.tile.Tile;
+
+/**
+ * Door object -- only used to construct doors for BoardMappings
+ * @author Malter
+ */
+public class Door {
+    
+    private int xRoom, yRoom, xTile, yTile;
+    private String dir;
+    /**
+     * constructor for making a door to be used by BoardMappings to link a tile to a room
+     * 
+     * 
+     * @param yRoom the y location of the room of the door
+     * @param xTile the x location of the outside tile
+     * @param yTile the y location of the outside tile
+     * @param dir the direction the player travels to go from the room to the outside tile
+     */
+    public Door(int yRoom, int xTile, int yTile, String dir){
+        this.yRoom = yRoom;
+        this.xTile = xTile;
+        this.yTile = yTile;
+        this.dir = dir;
+        if (xTile < 0 || yTile < 0 || yRoom < 0){
+            throw new ArrayIndexOutOfBoundsException("xTile, yTile and yRoom must be > 0");
+        }
+    }
+      
+    /**
+     * gets the room for the door
+     * @return the y identifier of the room
+     */
+    public int getRoomY(){
+        return yRoom;
+    }
+        
+    /**
+     * gets the x coordinate of the outside non room tile
+     * @return the x coordinate
+     */
+    public int getTileX(){
+        return xTile;
+    }
+    
+    /**
+     * gets the y coordinate of the outside non room tile
+     * @return the y coordinate
+     */
+    public int getTileY(){
+        return yTile;
+    }
+    
+    public String getDir(){
+        return dir;
+    }
+}
