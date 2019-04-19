9d5e4676f69d1724af7525efadc4908213a241c6
stephen-bracken <stephen.bracken20@gmail.com>
2019-04-19 18:44:48 +0100

    moved DoorTest to clue main test package

--- Clue/test/clue/DoorTest.java
+++ Clue/test/clue/DoorTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package clue.tile;
+package clue;
 
+import clue.Door;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
