57db56d353b2bf502428f67d2a55cb7579b82eb4
Marumaroo <hungbuiquang2@gmail.com>
2019-04-20 13:33:31 +0100

    Modified weaponSprite to use image to display which weapon it is

--- Clue/src/clue/client/WeaponSprite.java
+++ Clue/src/clue/client/WeaponSprite.java
@@ -5,7 +5,12 @@
  */
 package clue.client;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 
@@ -15,14 +20,23 @@ import javafx.scene.paint.Color;
  */
 public class WeaponSprite extends Label {
     private int positionX;
-    private int positionY;
-    
+    private int positionY;    
     
     public WeaponSprite(int x, int y, String sprite) {
         this.positionX = x;
         this.positionY = y;
-        setText(sprite);
-        setTextFill(Color.WHITE);
+        
+        Image image = null;
+        try {
+            image = new Image(new FileInputStream(new File(sprite)));
+        } catch(FileNotFoundException ex) {
+            
+        }
+        
+        ImageView weapon = new ImageView(image);
+        weapon.setFitHeight(32);
+        weapon.setFitWidth(32);
+        setGraphic(weapon);
     }
     
     public void move(int x, int y, StackPane[][] board, WeaponSprite weapon) {
