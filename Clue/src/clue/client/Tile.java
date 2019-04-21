d2a9b703ddaccdf780cf21493e1e8c2203fc0839
Marumaroo <hungbuiquang2@gmail.com>
2019-04-21 12:09:25 +0100

    removed unused code and imports

--- Clue/src/clue/client/Tile.java
+++ Clue/src/clue/client/Tile.java
@@ -5,24 +5,18 @@
  */
 package clue.client;
 
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.StrokeType;
 
 /**
  *
  * @author hungb
  */
 public class Tile extends Label {
-    
-    private PlayerSprite player;
     
     /**
      * Creates a Tile as a label
@@ -33,23 +27,7 @@ public class Tile extends Label {
         setPrefHeight(size);
 
     }
-    
-    /**
-     * Sets a player in this tile.
-     * @param player 
-     */
-    public void setPlayer(PlayerSprite player) {
-        this.player = player;
-    }
-    
-    /**
-     * Removes a player from this tile
-     * @param player the player  to remove from the tile
-     */
-    public void removePlayer(PlayerSprite player) {
-        this.player = null;
-    }
-    
+
     /**
      * Sets the colour of the tile
      * @param color the colour to change the tile to
