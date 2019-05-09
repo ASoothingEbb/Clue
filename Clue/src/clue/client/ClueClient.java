486b66c03d4a0220677592b7ede29956eca13d1d
Marumaroo <hungbuiquang2@gmail.com>
2019-05-09 21:24:41 +0100

    fixed null winner

--- Clue/src/clue/client/ClueClient.java
+++ Clue/src/clue/client/ClueClient.java
@@ -369,7 +369,7 @@ public class ClueClient extends Application {
             winner.append("No one won");
             winner.append("\n");
         } else {
-            winner.append(CardNameMap.get("Character" + winningPlayer));
+            winner.append(CardNameMap.get("character" + winningPlayer));
             winner.append("accused correctly and won!!!");
             winner.append("\n");
         }
